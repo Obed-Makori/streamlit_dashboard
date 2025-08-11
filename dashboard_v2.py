fb93c41134af7278f5c9d4d58248af842cbad720
Obed-Makori <makoriobed95@gmail.com>
2025-08-11 21:36:38 +0300

    day 10

--- dashboard_v2.py
+++ dashboard_v2.py
@@ -24,7 +24,7 @@ st.markdown("""
 )
 #pages
 def page2():
-     st.title("Prediction Form")\
+     st.title("Prediction Form")
      
 prediction=st.navigation([
      st.Page("PredictionForm.py", title="Page 1 for Now", icon="🩸"),\
@@ -32,7 +32,8 @@ prediction=st.navigation([
 ])
 # key variables
 df=pd.read_csv("diabetes.csv")
-selected=st.sidebar.selectbox('Explore Dataset', options=['Sample', 'First n'])
+selected=st.sidebar.selectbox('Explore Dataset', options=['Sample', 'First n', 'Dashboard'])
+selected2=st.sidebar.radio("Select Page", options=['Dataset', 'Dashboard'])
 
 if selected=="Sample":
     st.subheader("Diabetes Dataset")
@@ -40,3 +41,15 @@ if selected=="Sample":
         
 elif selected=="First n":
      st.data_editor(df)
+
+
+if selected2=="Dashboard":
+     columns=[column for column in df.columns]
+     selected_column=st.multiselect("KPIs", options=columns)
+     st.area_chart(data=df[selected_column])
+
+     selected_column2=st.selectbox("KPIs", options=columns)
+
+     if selected_column2=="Outcome":
+        #   st.plotly_chart(df, )
+          st.bar_chart(df[selected_column])
